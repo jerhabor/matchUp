dc6bcd7db855355a718c44111aea9ed79ae1b8f6
Jesse Erhabor <jess.erhabor@gmail.com>
2020-08-03 16:47:48 +0000

    FIX: Fixed a bug regarding the toggle switch for the dark/light modes. Changed the 'if' part of the function so it produces a value of the 'assets/css/style.css' and subsequently can be changed to 'assets/css/style-dark.css' when switch is clicked.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,15 @@
 $("document").ready(function() {
     $("#theme-toggle").click(function() {
         var lightDarkSwitch = $("#stylesheet");
-        if (lightDarkSwitch.attr("href", "assets/css/style.css")) {
+        if (lightDarkSwitch.attr("href") == "assets/css/style.css") {
             lightDarkSwitch.attr("href", "assets/css/style-dark.css");
+            console.log($("#stylesheet").attr("href"));
         }  else {
             lightDarkSwitch.attr("href", "assets/css/style.css");
-        };
+            console.log($("#stylesheet").attr("href"));
+        }
     });
-console.log($("#stylesheet").attr("href"));
+    // console.log($("#stylesheet").attr("href"));
     $( ".right-card" ).on("click", function() {
         $(this).toggleClass("flip");
       });
