178b3990aa7707a4413e9eecff210792661a8ef3
Jesse Erhabor <jess.erhabor@gmail.com>
2020-08-04 15:01:54 +0000

    FEAT: Assigned data values to right deck of cards to prepare match process.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,53 @@
-$("document").ready(function() {
+$(document).ready(function() {
+    var game = {
+        leftDeck: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16],
+        rightDeck: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16],
+        init: function() {
+            game.shuffleLeft()
+            game.shuffleRight();
+        },
+        shuffleLeft: function() {
+        var random = 0;
+        var temp = 0;
+        for (i = 1; i < game.leftDeck.length; i++) {
+            random = Math.round(Math.random() * i);
+            temp = game.leftDeck[i];
+            game.leftDeck[i] = game.leftDeck[random];
+            game.leftDeck[random] = temp;
+            };
+        console.log('Shuffled Left Deck Array: ' + game.leftDeck);
+        },
+        shuffleRight: function() {
+        var random = 0;
+        var temp = 0;
+        for (i = 1; i < game.rightDeck.length; i++) {
+            random = Math.round(Math.random() * i);
+            temp = game.rightDeck[i];
+            game.rightDeck[i] = game.rightDeck[random];
+            game.rightDeck[random] = temp;
+            };
+        game.assignRightDeck();
+        console.log('Shuffled Right Deck Array: ' + game.rightDeck);
+        },
+        assignRightDeck: function() {
+            $('.right-card').each(function(index) {
+            $(this).attr('data-card-value', game.rightDeck[index]);
+            });
+            game.clickHandlers();
+        },
+        clickHandlers: function() {
+            $( ".right-card" ).on("click", function() {
+                $(this).toggleClass("flip");
+            }); // Function for the flip animation of the cards when clicked
+            
+            $('.right-card').on('click', function() {
+            $(this).children('.card-front').html('<p>' + $(this).data('cardValue') + '</p>').addClass('selected');
+            });
+        }
+    };
+    game.init();
+
+
     $("#theme-toggle").click(function() {
         var lightDarkSwitch = $("#stylesheet");
         if (lightDarkSwitch.attr("href") == "assets/css/style.css") {
@@ -7,10 +56,7 @@ $("document").ready(function() {
         }  else {
             lightDarkSwitch.attr("href", "assets/css/style.css");
             console.log($("#stylesheet").attr("href"));
-        }
+        } // Function for the Light-Dark Theme Toggle
     });
-    // console.log($("#stylesheet").attr("href"));
-    $( ".right-card" ).on("click", function() {
-        $(this).toggleClass("flip");
-      });
+    
 });
